82c6c52dc4b83a4ba8f5d60a5216e9d2a6c18cea
Braian Rojas <braianrojas95@gmail.com>
2025-10-26 18:53:00 -0300

    feat: agrego clases Fecha/Autor

---
+++ Fecha.h
@@ -0,0 +1,25 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+class Fecha {
+private:
+    int dia;
+    int mes;
+    int anio;
+
+public:
+    Fecha();
+    Fecha(int d, int m, int a);
+
+    void cargar();
+    void mostrar();
+
+    void setDia(int d);
+    void setMes(int m);
+    void setAnio(int a);
+    int getDia();
+    int getMes();
+    int getAnio();
+};
+
+#endif
