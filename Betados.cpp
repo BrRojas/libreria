974434e039a10b65768b544d8da5f7787327080b
agustintrejo52-ux <agustintrejo52@gmail.com>
2025-11-13 20:54:45 -0300

    cambio estructural
    Cambio estructural de betados, se genera para distribuir tareas de manager y menu.

--- Betados.cpp
+++ Betados.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 Vetados::Vetados() {
     IdVetados = 0;
-    RestaPuntos = -5;
 }
 
 void Vetados::setIdVetados(int id) {
@@ -16,56 +15,9 @@ void Vetados::setIdVetados(int id) {
         IdVetados = 0;
     }
 }
-void Vetados::setRestaPuntos(int puntos){
-    if (puntos >= 5) {
-            RestaPuntos += puntos;
-        } else {
-            cout << "Error: no hay suficientes puntos para restar. ";
-        }
-    }
+
 
 int Vetados::getIdVetados (){
     return IdVetados;
     }
 
-int Vetados::getRestaPuntos(){
-    return RestaPuntos;
-    }
-
-void Vetados::CargarVetados(){
-    int id;
-    cout << "Ingrese ID del vetado: ";
-    cin >> id;
-
-    setIdVetados(id);
-
-    if(IdVetados != 0) {
-        FILE* archivo = fopen("Vetados.dat", "ab");
-        if(archivo != nullptr) {
-            fwrite(&IdVetados, sizeof(int), 1, archivo);
-            fclose(archivo);
-            cout << "Vetado agregado y guardado correctamente." << endl;
-        } else {
-            cout << "No se pudo abrir el archivo." << endl;
-        }
-    }
-}
-
-void Vetados::MostrarVetados(){
-    FILE* archivo = fopen("Vetados.dat", "rb");
-    if(archivo != nullptr) {
-        int id;
-        int contador = 0;
-        cout << "Lista de vetados:" << endl;
-        while(fread(&id, sizeof(int), 1, archivo) == 1) {
-            contador++;
-            cout << contador << ". ID: " << id << endl;
-        }
-        if(contador == 0) {
-            cout << "No hay vetados cargados." << endl;
-        }
-        fclose(archivo);
-    } else {
-        cout << "No se pudo abrir el archivo." << endl;
-    }
-    }
