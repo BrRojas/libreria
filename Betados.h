4eb404294c4b98c313bf04f5475b4bdf36f5b747
agustintrejo52-ux <agustintrejo52@gmail.com>
2025-11-13 20:55:24 -0300

    cambio estructural

--- Betados.h
+++ Betados.h
@@ -3,13 +3,8 @@
 class Vetados{
     private:
         int IdVetados;
-        int RestaPuntos;
     public:
         Vetados();
         void setIdVetados(int);
-        void setRestaPuntos(int);
         int getIdVetados();
-        int getRestaPuntos();
-        void CargarVetados();
-        void MostrarVetados();
 };
