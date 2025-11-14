66dd0b7c949d21312dd45e1a27bb1aac44774c06
agustintrejo52-ux <agustintrejo52@gmail.com>
2025-11-14 00:15:10 -0300

    reestructuracion de socio

--- CSocio.h
+++ CSocio.h
@@ -3,22 +3,24 @@
 class Socio{
     private:
         int IdSocio;
+        char Nombre [30];
         char Categoria[30];
         char Telefono[15];
-        bool Estado;
-        bool IncluyeDonacion;
+        char Estado;
+        char IncluyeDonacion;
     public:
         Socio();
         void setIdSocio(int);
+        void setNombre(const char*);
         void setCategoria(char*);
         void setTelefono(char*);
-        void setEstado(bool);
-        void setIncluyeDonacion(bool);
+        void setEstado(char);
+        void setIncluyeDonacion(char);
         int getIdSocio();
+        const char* getNombre();
         char *getCategoria();
         char *getTelefono();
-        bool getEstado();
-        bool getIncluyeDonacion();
-        void Cargar();
-        void Mostrar();
+        char getEstado();
+        char getIncluyeDonacion();
+
 };
