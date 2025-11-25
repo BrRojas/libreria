83445029fcdfe5a82a81163be023e7874fd0835f
agustintrejo52-ux <agustintrejo52@gmail.com>
2025-11-25 20:34:30 -0300

    Update Prestamo.cpp

--- Prestamo.cpp
+++ Prestamo.cpp
@@ -6,69 +6,19 @@ using namespace std;
 
     //Fecha maxima agregar, fecha limite 1 mes
 
-    // Constructor
-    Prestamo::Prestamo(int idPrestamo, int idSocio, char* isBn, Fecha fechaPrestamo, Fecha fechaDevolucion, bool estado) {
-        this->idPrestamo = idPrestamo;
-        this->idSocio = idSocio;
-        strcpy(this->isBn, isBn);
-        this->fechaPrestamo = fechaPrestamo;
-        this->fechaDevolucion = fechaDevolucion;
-        this->estado = estado;
-    }
-
     Prestamo::Prestamo(){
         return;
-    }
-
-    int Prestamo::Cargar() {
-        int idPrestamo, idSocio; // id del prestamo deberia generarse automaticamente
-        char isBn[20];
-        Fecha fechaPrestamo, fechaDevolucion;
-        bool estado;
-        cout << "Ingrese el Id del socio: ";
-        cin >> idSocio;
-        cin.ignore();
-        cout << "Ingrese el isBn: ";
-        cin.getline(isBn, 20);
-        cout << "Ingrese la fecha del prestamo: ";
-        fechaPrestamo.cargar();
-        cout << "Ingrese la fecha de devolucion: ";
-        fechaDevolucion.cargar();
-
-        setIdSocio(idSocio);
-        setIsBn(isBn);
-        setFechaPrestamo(fechaPrestamo);
-        setFechaDevolucion(fechaDevolucion);
-        setEstado(true);
-        long dias = fechaPrestamo.diasEntre(fechaDevolucion);
-        SocioManager sm;
-        if (dias <= 31) {
-        sm.SumarPuntos(idSocio, 10);
-        }
-        else {
-        sm.SumarPuntos(idSocio, -20);
-        }
-        return 0;
-    }
-
-    void Prestamo::Mostrar() {
-        cout << "IDPrestamo: " << getIdPrestamo() << endl;
-        cout << "Fecha de Prestamo: ";
-        getFechaPrestamo().mostrar();
-        cout << endl;
-        cout << "Fecha de devolucion: ";
-        getFechaDevolucion().mostrar();
-        cout << endl;
-        cout << "Estado: " << getEstado() << endl;
-    }
-
-    void Prestamo::MostrarPrestamos() {
-
     }
 
     // Getters
     int Prestamo::getIdPrestamo() {
         return idPrestamo;
+    }
+    int Prestamo::getIdSocio() {
+        return idSocio;
+    }
+    char* Prestamo::getIsBn() {
+        return isBn;
     }
     Fecha Prestamo::getFechaPrestamo() {
         return fechaPrestamo;
